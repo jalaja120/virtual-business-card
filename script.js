c8ba64dc5836a44cd9a706009f091321d2a52d61
jalaja <jaanu3526@gmail.com>
2026-02-23 17:43:17 +0530

    Initial commit - Virtual Business Card

---
+++ script.js
@@ -0,0 +1,15 @@
+function showMessage() {
+    alert("Thank you for visiting my digital card!");
+}
+
+const toggleBtn = document.getElementById("themeToggle");
+
+toggleBtn.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        toggleBtn.textContent = "☀️";
+    } else {
+        toggleBtn.textContent = "🌙";
+    }
+});
